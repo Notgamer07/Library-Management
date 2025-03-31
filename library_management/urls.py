3c23561692e0214e9dc28a5ba1a1b7f62565c7b6
Notgamer07 <tyjagrath@gmail.com>
2025-04-01 01:13:59 +0530

    Updated books.html to fit the theme as well as added horizontal scroll bar.

--- library_management/urls.py
+++ library_management/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
- 
+from django.http import HttpResponseNotFound
+
  
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('library.urls')),
+    path('favicon.ico', lambda request: HttpResponseNotFound()),
 ]
